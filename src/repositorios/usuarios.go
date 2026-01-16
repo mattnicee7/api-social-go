b9192da81e7658a3603f881fb44ff302c9080b28
mattnicee7 <mattxnice@gmail.com>
2026-01-16 03:00:42 -0300

    repositorios

---
+++ src/repositorios/usuarios.go
@@ -0,0 +1,21 @@
+package repositorios
+
+import (
+	"api-social-go/src/modelos"
+	"database/sql"
+)
+
+// Usuarios representa um repositorio de usuarios
+type Usuarios struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDeUsuarios cria um repositorio de usuarios
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
+}
+
+// Criar insere um usuario no banco de dados
+func (u Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	return 0, nil
+}
