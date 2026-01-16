7db3189d06cb61f67ecef123544a452c6e65d183
mattnicee7 <mattxnice@gmail.com>
2026-01-16 03:12:17 -0300

    fixed db.close

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -28,6 +28,8 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(erro)
 	}
 
+	defer db.Close()
+
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
 	usuarioID, erro := repositorio.Criar(usuario)
 	if erro != nil {
