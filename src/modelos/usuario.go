059611d0ddadec4950a2e466564f1928d070acf2
mattnicee7 <mattxnice@gmail.com>
2026-01-18 03:53:37 -0300

    validacao de email

--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuario representa um usuario utilizando a rede social
@@ -37,6 +39,10 @@ func (usuario *Usuario) validar(etapa string) error {
 
 	if usuario.Email == "" {
 		return errors.New("O email é obrigatório e não pode estar em branco.")
+	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O email inserido é invalido")
 	}
 
 	if etapa == "cadastro" && usuario.Senha == "" {
