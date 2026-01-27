f410a6c29ed5560889f9ac7eee18ac0829ca5f9b
mattnicee7 <mattxnice@gmail.com>
2026-01-27 19:32:09 -0300

    seguir usuario

--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -36,4 +36,10 @@ var rotasUsuarios = []Rota{
 		Funcao:           controllers.DeletarUsuario,
 		RequerAutenticao: true,
 	},
+	{
+		URI:              "/usuarios/{usuarioId}/seguir",
+		Metodo:           http.MethodPost,
+		Funcao:           controllers.SeguirUsuario,
+		RequerAutenticao: true,
+	},
 }
